c542d9af941a1b3011ad7a97a1123f386118d696
heshan umayanga <heshanu97@gmail.com>
2022-02-19 00:38:11 +0530

    added directive answerHeightlight

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -1,3 +1,4 @@
+/*
 import { Component, OnInit } from '@angular/core';
 import { FormGroup,FormControl,Validators, FormsModule } from '@angular/forms';
 import {MathValidators} from '../math-validators';
@@ -34,11 +35,12 @@ export class EquationComponent implements OnInit {
     return this.mathForm.value.b;
   }
 
+
   ngOnInit() {
     this.mathForm.statusChanges
       .pipe(
         filter(value => value ==="VALID"),
-        delay(100),
+        delay(500),
         scan((acc) => {
             return{
               numberSolved:acc.numberSolved+1,
@@ -58,5 +60,68 @@ export class EquationComponent implements OnInit {
           answer: ''
         });
       });
+  }
+}
+*/
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { delay, filter, scan } from 'rxjs/operators';
+import { MathValidators } from '../math-validators';
+
+@Component({
+  selector: 'app-equation',
+  templateUrl: './equation.component.html',
+  styleUrls: ['./equation.component.css']
+})
+export class EquationComponent implements OnInit {
+  secondsPerSolution = 0;
+  mathForm = new FormGroup(
+    {
+      a: new FormControl(this.randomNumber()),
+      b: new FormControl(this.randomNumber()),
+      answer: new FormControl('')
+    },
+    [MathValidators.addition('answer', 'a', 'b')]
+  );
+
+  constructor() {}
+
+  get a() {
+    return this.mathForm.value.a;
+  }
+
+  get b() {
+    return this.mathForm.value.b;
+  }
+
+  ngOnInit() {
+    this.mathForm.statusChanges
+      .pipe(
+        filter(value => value === 'VALID'),
+        delay(150),
+        scan(
+          acc => {
+            return {
+              numberSolved: acc.numberSolved + 1,
+              startTime: acc.startTime
+            };
+          },
+          { numberSolved: 0, startTime: new Date() }
+        )
+      )
+      .subscribe(({ numberSolved, startTime }) => {
+        this.secondsPerSolution =
+          (new Date().getTime() - startTime.getTime()) / numberSolved / 1000;
+
+        this.mathForm.setValue({
+          a: this.randomNumber(),
+          b: this.randomNumber(),
+          answer: ''
+        });
+      });
+  }
+
+  randomNumber() {
+    return Math.floor(Math.random() * 10);
   }
 }
