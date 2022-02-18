f9960378c67285f392342835f5b0cff5e9d47134
heshan umayanga <heshanu97@gmail.com>
2022-02-18 10:45:01 +0530

    Added equation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import {ReactiveFormsModule} from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EquationComponent } from './equation/equation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EquationComponent
   ],
   imports: [
     BrowserModule,
