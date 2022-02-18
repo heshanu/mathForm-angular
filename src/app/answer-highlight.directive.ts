8a75ab9ef0911d7474a9490da7ae854496ac2912
heshan umayanga <heshanu97@gmail.com>
2022-02-19 01:25:57 +0530

    finish with bug

--- src/app/answer-highlight.directive.ts
+++ src/app/answer-highlight.directive.ts
@@ -1,10 +1,26 @@
-import { Directive } from '@angular/core';
+import { Directive,ElementRef } from '@angular/core';
+import { NgControl } from '@angular/forms';
+import {map,filter} from 'rxjs/operators';
 
 @Directive({
   selector: '[appAnswerHighlight]'
 })
+
 export class AnswerHighlightDirective {
+  constructor(private el:ElementRef,private controlName:NgControl) {
+    
+  }
 
-  constructor() { }
-
+  ngOnInit(){
+    this.controlName.control?.parent?.valueChanges.pipe(
+      map(({a,b,answer})=> Math.abs(a+b-answer)/(a+b)),
+      filter(value=>value<0.2)
+    ).subscribe((value)=>{
+        if(value<0.2){
+          this.el.nativeElement.classList.add('CLOSE');
+        }else{
+          this.el.nativeElement.classList.remove('CLOSE');
+        }
+      })
+  }
 }
